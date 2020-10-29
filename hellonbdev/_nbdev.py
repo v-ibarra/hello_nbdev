c2118bb0fe29bf571c558336d53725ded5d8a5cc
v-ibarra <victor.ibarra.encinas@gmail.com>
2020-10-29 00:45:13 -0700

    init commit

--- hellonbdev/_nbdev.py
+++ hellonbdev/_nbdev.py
@@ -3,7 +3,8 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"say_hello": "00_core.ipynb",
-         "HelloSayer": "00_core.ipynb"}
+         "HelloSayer": "00_core.ipynb",
+         "proportion": "00_core.ipynb"}
 
 modules = ["core.py"]
 
